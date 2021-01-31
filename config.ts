209ad69b312f4755f502d2cfd194e9d1052751c3
zhangtianli2006 <zhangtianli2006@163.com>
2021-01-31 15:04:58 +0800

    [feature] allow no config

--- config.ts
+++ config.ts
@@ -6,11 +6,13 @@ export interface Config {
 };
 
 export function fetch_config(): Config {
+    const default_config: Config = { port: 3000 };
+
     const config_path = process.env.cfg || './data-server.config.json';
+
+    if (!fs.existsSync(config_path)) { return default_config; }
     const content = fs.readFileSync(config_path);
     let config = JSON.parse(content.toString());
 
-    return use_default_config(config, {
-        port: 3000,
-    });
+    return use_default_config(config, default_config);
 }
