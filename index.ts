25fb2c5fd5b7eb1c6d96926486b6f341505246ed
zhangtianli2006 <zhangtianli2006@163.com>
2021-01-31 16:26:42 +0800

    [fix] change ordering

--- index.ts
+++ index.ts
@@ -6,7 +6,6 @@ import { find_data } from './list-data';
 const config = fetch_config();
 
 const app = express();
-app.use(express.static(config.testdata_path), serveIndex(config.testdata_path, { icons: true }));
 
 app.get('/api/data/:id', (req, res) => {
     const id = parseInt(req.params.id);
@@ -14,9 +13,11 @@ app.get('/api/data/:id', (req, res) => {
         res.status(400).end();
         return;
     }
-
+    
     res.type('application/json').send(JSON.stringify({ res: find_data(config.testdata_path, id) })).end();
 });
+
+app.use(express.static(config.testdata_path), serveIndex(config.testdata_path, { icons: true }));
 
 app.listen(config.port, () => {
     console.log(`Serving on port ${config.port}`);
