4ddb92e0b96f528318d021b672135d4bca37f6b4
gzezFISHER <fisher@mail.soj.ac.cn>
2021-02-01 15:32:21 +0800

    [feature] add alive and capacity api

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { fetch_config } from './config';
 import serveIndex from 'serve-index';
 import express from 'express';
 import { find_data } from './list-data';
-import { calc_size } from './utilities';
+import { calc_size, get_disk_capacity } from './utilities';
 
 const config = fetch_config();
 
@@ -20,6 +20,14 @@ app.get('/api/data/:id', (req, res) => {
 
 app.get('/api/size', async (req, res) => {
     res.json({ res: await calc_size(config.testdata_path) }).end();
+});
+
+app.get('/api/capacity', async (req, res) => {
+    res.json({ res: await get_disk_capacity(config.testdata_path) }).end();
+});
+
+app.get('/api/alive', (req, res) => {
+    res.send('true').end();
 });
 
 app.use(express.static(config.testdata_path), serveIndex(config.testdata_path, { icons: true }));
