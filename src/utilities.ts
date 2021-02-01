e1bbfcf06ada1106dd3bc8a07774a6beb9b6d3de
gzezFISHER <fisher@mail.soj.ac.cn>
2021-02-01 15:32:21 +0800

    [feature] add alive and capacity api

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,5 +1,6 @@
 import fs = require('fs');
 import path = require('path');
+import diskusage = require('diskusage');
 
 export function use_default_config(a, d) {
     for (let i in d) {
@@ -18,3 +19,13 @@ export async function calc_size(target: string) {
     }
     return Math.ceil(size / (1024 * 1024));
 };
+
+export async function get_disk_capacity(path: string) {
+    try {
+        const { available } = await diskusage.check(path);
+        return Math.ceil(available / (1024 * 1024)) - 5000;
+    } catch (err) {
+        console.error(err);
+        return 0;
+    }
+}
